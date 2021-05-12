e219ca85d16a056d5906166d1dd3fdcbe2ec741f
Luca Xue <lxue02@gmail.com>
2021-05-12 11:51:39 +0100

    styled components ssr

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,33 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+import React from 'react';
+import { ServerStyleSheet } from 'styled-components';
 
 class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
   render() {
     return (
       <Html>
@@ -10,7 +37,6 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Prata&family=Assistant:wght@200;300;400;500;600;700;800&display=swap"
             rel="stylesheet"
           />
-          {/* <link rel="icon" href="/favicon.ico" /> */}
         </Head>
         <body>
           <Main />
